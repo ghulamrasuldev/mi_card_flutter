739d29ec2bd1b1a528d428b7afea628a0c5abbbb
ghulam-rasool-dev <ghulam.rasool.freelance@gmail.com>
2021-01-18 14:14:44 +0500

    created stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.deepPurpleAccent,
+        backgroundColor: Colors.green,
         body: Container(),
       ),
     );
