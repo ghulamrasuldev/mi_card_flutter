d0f71ae468e95b65e0c890cabf6fc3dfb1e67284
ghulam-rasool-dev <ghulam.rasool.freelance@gmail.com>
2021-01-18 18:32:59 +0500

    final commit of 18 Jan 2021

--- lib/main.dart
+++ lib/main.dart
@@ -10,13 +10,31 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.green,
+        backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
             children: <Widget>[
               CircleAvatar(
                 radius: 50,
                 backgroundImage: AssetImage("images/gama.jpg"),
+              ),
+              Text(
+                'Ghulam Rasool',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2,
+                  color: Colors.white.withOpacity(.6),
+                ),
               ),
             ],
           ),
