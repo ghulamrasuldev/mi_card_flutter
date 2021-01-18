1bd0525b019318a610ac33a113dd0fbee1722b77
ghulam-rasool-dev <ghulam.rasool.freelance@gmail.com>
2021-01-18 17:50:09 +0500

    ADDED MY PHOTO

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(
-    MyApp()
-  );
+  runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.green,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage("images/gama.jpg"),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
