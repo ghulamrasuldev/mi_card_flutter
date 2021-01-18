6cd36a2a21dffa08771a3d3aba6de206239c586d
ghulam-rasool-dev <ghulam.rasool.freelance@gmail.com>
2021-01-18 14:13:50 +0500

    created stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyApp()
+  );
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.deepPurpleAccent,
         body: Container(),
       ),
-    ),
-  );
+    );
+  }
 }
