586b584b77f2269e8848373cf5c1e0322ff19ab1
ghulam-rasool-dev <ghulam.rasool.freelance@gmail.com>
2021-01-19 14:47:00 +0500

    final commit of 18 Jan 2021

--- lib/main.dart
+++ lib/main.dart
@@ -10,34 +10,79 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-          child: Column(
-            children: <Widget>[
-              CircleAvatar(
-                radius: 50,
-                backgroundImage: AssetImage("images/gama.jpg"),
-              ),
-              Text(
-                'Ghulam Rasool',
-                style: TextStyle(
-                  fontFamily: 'Pacifico',
-                  fontSize: 40,
-                  color: Colors.white,
-                ),
-              ),
-              Text(
-                'FLUTTER DEVELOPER',
-                style: TextStyle(
-                  fontFamily: 'SourceSansPro',
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 2,
-                  color: Colors.white.withOpacity(.6),
-                ),
-              ),
-            ],
+        appBar: AppBar(
+          backgroundColor: Colors.teal[800],
+          title: Center(
+            child: Text(
+              "Business Card",
+              style: TextStyle(fontFamily: "SourceSansPro"),
+            ),
           ),
+        ),
+        backgroundColor: Colors.teal.shade600,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage("images/gama.jpg"),
+            ),
+            Text(
+              'Ghulam Rasool',
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                fontSize: 40,
+                color: Colors.white,
+              ),
+            ),
+            Text(
+              'FLUTTER DEVELOPER',
+              style: TextStyle(
+                fontFamily: 'SourceSansPro',
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2,
+                color: Colors.white.withOpacity(.6),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  "+923204970584",
+                  style: TextStyle(
+                      fontFamily: "SourceSansPro",
+                      fontSize: 14,
+                      color: Colors.teal,
+                      fontWeight: FontWeight.normal),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  "ghulam.rasool.freelance@gmail.com",
+                  style: TextStyle(
+                      fontFamily: "SourceSansPro",
+                      fontSize: 14,
+                      color: Colors.teal,
+                      fontWeight: FontWeight.normal),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            )
+          ],
         ),
       ),
     );
